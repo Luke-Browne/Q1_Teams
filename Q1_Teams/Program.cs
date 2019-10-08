e978b41c95d0ac14362dcd8ca8188bf8e6bdfe31
Luke Browne <S00187306@mail.itsligo.ie>
2019-10-08 12:07:37 +0100

    Add project files.

---
+++ Q1_Teams/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_Teams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
